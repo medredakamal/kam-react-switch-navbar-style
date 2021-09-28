68f53322f2186d54c2dc91bd618e4029cce5e0a7
Name <myemail@gmail.com>
2021-09-28 22:53:29 +0100

    Updates

---
+++ precache-manifest.94c68be39d9f4e123b46199f8a8c9c1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec0844da44ee0a6c50a7145d7723b36",
+    "url": "/kam-react-switch-navbar-style/index.html"
+  },
+  {
+    "revision": "d7d79c04d788f2b3bae1",
+    "url": "/kam-react-switch-navbar-style/static/css/2.d6bd1c3d.chunk.css"
+  },
+  {
+    "revision": "bca5f90c63d6b8a8cfe1",
+    "url": "/kam-react-switch-navbar-style/static/css/main.11f10efb.chunk.css"
+  },
+  {
+    "revision": "d7d79c04d788f2b3bae1",
+    "url": "/kam-react-switch-navbar-style/static/js/2.6aae9e0c.chunk.js"
+  },
+  {
+    "revision": "25d70d1659cfdbd5296076c6e3f0cd8e",
+    "url": "/kam-react-switch-navbar-style/static/js/2.6aae9e0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bca5f90c63d6b8a8cfe1",
+    "url": "/kam-react-switch-navbar-style/static/js/main.2339486d.chunk.js"
+  },
+  {
+    "revision": "ed934bcf406b98a519ea",
+    "url": "/kam-react-switch-navbar-style/static/js/runtime-main.ac93d314.js"
+  },
+  {
+    "revision": "912ea3e09f9ac7f3414cc5fc51e8fd47",
+    "url": "/kam-react-switch-navbar-style/static/media/medredakamal.912ea3e0.jpeg"
+  }
+]);
